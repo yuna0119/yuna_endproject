8e007fca8b06f2d385be275ac139bbe75031c176
chengyi <0524eric@gmail.com>
2023-12-10 15:00:51 +0800

    Initial Commit

---
+++ yuna_endproject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  yuna_endproject
+//
+//  Created by user05 on 2023/12/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ yuna_endproject/yuna_endprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  yuna_endprojectApp.swift
+//  yuna_endproject
+//
+//  Created by user05 on 2023/12/10.
+//
+
+import SwiftUI
+
+@main
+struct yuna_endprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
